a24beaf2b89ca5e14856d3f301be2d809fef19bd
pilmenuch <65299759+pilmenuch@users.noreply.github.com>
2020-06-29 23:18:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1><font color=red>Congratulations <br><font color=white>From it-academy!!!</font></h1>
+        <h1><font color=red>Congratulations <br><font color=black>From it-academy!!!</font></h1>
         <p>Your    AWS Elastic Beanstalk  <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
